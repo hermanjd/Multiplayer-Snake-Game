8f2cc9cebdd4939a04e99f9ee091b5ed1b9cfbef
Hekkii <hekkiiwalters@gmail.com>
2017-12-29 01:01:07 +0100

    ignore the nodes
    but not the locks

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(5000, function()){
+  console.log('starting the fuccen server on port 50000000000');
+}
